31f350db438debcf27af3bbbfe9190686be60182
miku3984 <152942475+leehamin-miku@users.noreply.github.com>
2025-10-14 18:43:18 +0900

    Update websocket_server.c

--- tinyIoT-main/source/server/websocket_server.c
+++ tinyIoT-main/source/server/websocket_server.c
@@ -275,8 +275,8 @@ void initialize_websocket_server() {
     memset(&info, 0, sizeof(info));
     info.port = 8081;          // 일반 WS 포트
     info.protocols = protocols; // 미리 정의된 프로토콜 배열
-    info.extensions = lws_get_internal_extensions();  // permessage-deflate 포함
-    //info.options = LWS_SERVER_OPTION_DISABLE_IPV6 | LWS_SERVER_OPTION_SKIP_SERVER_PERMESSAGE_DEFLATE;
+    //info.extensions = lws_get_internal_extensions();  // permessage-deflate 포함
+    info.options |= LWS_SERVER_OPTION_SKIP_SERVER_PERMESSAGE_DEFLATE;
     info.gid = -1;
     info.uid = -1;
 
